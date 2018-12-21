50f0c654427169f913ec2d5c6903ee161e85f17b
whhong <751997661@qq.com>
2018-12-21 07:38:18 +0000

    Add travis ci config file.

--- src/Weather.php
+++ src/Weather.php
@@ -58,4 +58,15 @@ class Weather
             throw new HttpException($e->getMessage(), $e->getCode(), $e);
         }
     }
+
+
+    public function getLiveWeather($city, $format = 'json')
+    {
+        return $this->getWeather($city, 'base', $format);
+    }
+
+    public function getForecastsWeather($city, $format = 'json')
+    {
+        return $this->getWeather($city, 'all', $format);
+    }
 }
